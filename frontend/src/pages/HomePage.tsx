9140b79d3c66007532de7ea8aaf8d9b1541db9fb
Rogelio Olarte <galaxino12@gmail.com>
2024-07-19 03:01:06 -0500

    Update HomePage.tsx

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,5 +1,7 @@
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
   return (
-    <div>HomePage</div>
+    <div>HomePage<Link to="/register">Register</Link></div>
   )
 }
